79e52bc2e91c91b17f6a5aa92354eac044dc2936
blessi30ng <blessingchidewu3@gmail.com>
2024-06-24 06:32:43 -0700

    lol

--- models/city.py
+++ models/city.py
@@ -5,5 +5,9 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
+    ___tablename__ = "cities"
+    name = Column(string(128), nullable=False)
+    state_id = Column(string(60), ForeignKey('states.id'), nullable=False)
+
     state_id = ""
     name = ""
